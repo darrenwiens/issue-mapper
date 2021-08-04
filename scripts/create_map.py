065d9c855b991e340186f82005177208b3c5a925
Darren Wiens <dkwiens@gmail.com>
2021-08-04 13:48:53 -0700

    fix issue body

--- scripts/create_map.py
+++ scripts/create_map.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 ISSUE_NUMBER = os.getenv("ISSUE_NUMBER")
-ISSUE_BODY = os.getenv("ISSUE_NUMBER")
+ISSUE_BODY = os.getenv("ISSUE_BODY")
 
 print(f"ISSUE_NUMBER: {ISSUE_NUMBER}")
 print(f"ISSUE_BODY: {ISSUE_BODY}")
